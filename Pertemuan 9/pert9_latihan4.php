263eefc398a4266ea7463103e8a7c2d287ce4f06
Muhammad Syah Rizal <79929326+syahrizzal06@users.noreply.github.com>
2021-03-16 20:30:20 +0700

    Create pert9_latihan4.php

---
+++ Pertemuan 9/pert9_latihan4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tanggal</title>
+	<style type="text/css">
+		h3	{
+			color: blue;
+		}
+	</style>
+</head>
+<body>
+	<font size="10px">
+		<h3>FUNGSI DATE</h3>
+		<h3>ANRE FRANCISCUS</h3>
+	<?php 
+	echo "Sekarang Tanggal";
+	echo date('d-F-Y');
+	echo "<br> dan jam";
+	echo date('h:i:s A');
+	 ?>
+	</font>
+</body>
+</html>
