9a0297dfc2f8ef9162ec501c271375f694888c1d
Muhammad Syah Rizal <79929326+syahrizzal06@users.noreply.github.com>
2021-03-25 16:10:18 +0700

    Add files via upload

--- Pertemuan 12/pert12_latihan1.php
+++ Pertemuan 12/pert12_latihan1.php
@@ -10,7 +10,7 @@ if (!$con) {
 $query =mysqli_query($con, "UPDATE tbl_mhs SET age ='36' WHERE firstName = 'Karina'AND lastName = 'Suwandi' ");
 if ($query) {
 	echo "<font color='gray' align='center'>
-	<h1>DATA SUKSES DIHAPUS</h1>
+	<h1>DATA SUKSES DIUBAH</h1>
 	</font>";
 }
 
